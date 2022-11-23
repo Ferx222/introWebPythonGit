8002e6739c250628fa94c365e63597633e042ed2
Meirelles Brito <94622046+Ferx222@users.noreply.github.com>
2022-11-23 10:38:14 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template
 
-from flask import Flask
-app=Flask("alo mundo")
+
+app = Flask("Olá mundo")
+
+@app.route("/alunos")
 @app.route("/")
+
 def alunos():
-   return render_template("hello.html")
+    return render_template("hello.html")
+
