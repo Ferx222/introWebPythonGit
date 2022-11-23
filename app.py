8df9d00ecb4e6e39a3cf2ef21b5f98c753cdeb57
Meirelles Brito <94622046+Ferx222@users.noreply.github.com>
2022-11-23 10:51:10 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
+import sqlite3
 
+DATABASE = "banco.bd"
+SECRET_KEY = "1234"
 
 app = Flask("Olá mundo")
 
-@app.route("/alunos")
-@app.route("/")
+app.config.from_object(__name__)
 
-def alunos():
-    return render_template("hello.html")
+def conectar():
+    return sqlite3.connect(DATABASE)
+
+@app.before_request 
+def before_request():
+    g.bd = conectar()
+
+@app.teardown_request
+def teardown_request(f):
+    g.bd.close()
+
+@app.route("/")
+def exibir_posts():
+    sql  = "SELECT titulo, texto, data_criacao FROM posts ORDER BY id DESC" 
+    resultado = g.bd.execute(sql)
+    posts = []
+
+    for titulo, texto, data_criacao in resultado.fetchall():
+        posts.append({
+            "titulo": titulo,
+            "texto": texto,
+            "data_criacao": data_criacao
+        })
+    return render_template("hello.html", post = posts)
 
